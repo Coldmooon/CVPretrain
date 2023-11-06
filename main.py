801d435a71d6196503f15e2ba50467aff23303e9
Coldmooon <howtoliy@gmail.com>
2023-11-06 19:32:58 +0800

    Separate model creating from the main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import torch.nn.parallel
 import torch.optim
 import torch.utils.data
 import torch.utils.data.distributed
-import torchvision.models as models
+
 
 from torch.optim.lr_scheduler import MultiStepLR
 from torch.optim.lr_scheduler import ChainedScheduler
@@ -22,6 +22,7 @@ from opts import ArgumentParser
 from datasets import dataloader as Dataloader
 from train import Trainer
 from checkpoints import Checkpoints
+from models.init import Model
 
 best_acc1 = 0
 best_acc5 = 0
@@ -112,62 +113,10 @@ def main_worker(gpu, ngpus_per_node, args):
     is_master = args.rank == 0
     do_log = run is not None
 
-    # create model
-    if args.pretrained:
-        print("=> using pre-trained model '{}'".format(args.arch))
-        model = models.__dict__[args.arch](pretrained=True)
-    else:
-        print("=> creating model '{}'".format(args.arch))
-        model = models.__dict__[args.arch]()
 
-    if not torch.cuda.is_available() and not torch.backends.mps.is_available():
-        print('using CPU, this will be slow')
-    elif args.distributed:
-        # For multiprocessing distributed, DistributedDataParallel constructor
-        # should always set the single device scope, otherwise,
-        # DistributedDataParallel will use all available devices.
-        if torch.cuda.is_available():
-            if args.gpu is not None:
-                print('args.gpu:::::::', args.gpu)
-                torch.cuda.set_device(args.gpu)
-                model.cuda(args.gpu)
-                # When using a single GPU per process and per
-                # DistributedDataParallel, we need to divide the batch size
-                # ourselves based on the total number of GPUs of the current node.
-                args.batch_size = int(args.batch_size / ngpus_per_node)
-                args.workers = int((args.workers + ngpus_per_node - 1) / ngpus_per_node)
-                if args.compiled == 1:
-                    model = torch.compile(model)   
-
-                s = torch.cuda.Stream()
-                s.wait_stream(torch.cuda.current_stream())
-                with torch.cuda.stream(s):
-                    model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
-                torch.cuda.current_stream().wait_stream(s)
-                # model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
-            else:
-                model.cuda()
-
-                if args.compiled == 1:
-                    model = torch.compile(model)   
-
-                # DistributedDataParallel will divide and allocate batch_size to all
-                # available GPUs if device_ids are not set
-                model = torch.nn.parallel.DistributedDataParallel(model)
-    elif args.gpu is not None and torch.cuda.is_available():
-        torch.cuda.set_device(args.gpu)
-        model = model.cuda(args.gpu)
-    elif torch.backends.mps.is_available():
-        device = torch.device("mps")
-        model = model.to(device)
-    else:
-        # DataParallel will divide and allocate batch_size to all available GPUs
-        if args.arch.startswith('alexnet') or args.arch.startswith('vgg'):
-            model.features = torch.nn.DataParallel(model.features)
-            model.cuda()
-        else:
-            model = torch.nn.DataParallel(model).cuda()
-
+    # create modle
+    modeling = Model(args)
+    model = modeling.create(ngpus_per_node)
 
     if torch.cuda.is_available():
         if args.gpu:
---
+++ models/init.py
@@ -0,0 +1,70 @@
+import torch
+import torchvision.models as models
+
+class Model:
+    def __init__(self, args):
+        self.args = args
+
+
+    def create(self, ngpus_per_node):
+        model = self.setup(ngpus_per_node)
+        return model
+
+
+    def setup(self, ngpus_per_node, model=None):
+
+        if self.args.pretrained:
+            print("=> using pre-trained model '{}'".format(self.args.arch))
+            model = models.__dict__[self.args.arch](pretrained=True)
+        else:
+            print("=> creating model '{}'".format(self.args.arch))
+            model = models.__dict__[self.args.arch]()
+
+        if not torch.cuda.is_available() and not torch.backends.mps.is_available():
+            print('using CPU, this will be slow')
+        elif self.args.distributed:
+            # For multiprocessing distributed, DistributedDataParallel constructor
+            # should always set the single device scope, otherwise,
+            # DistributedDataParallel will use all available devices.
+            if torch.cuda.is_available():
+                if self.args.gpu is not None:
+                    print('args.gpu:::::::', self.args.gpu)
+                    torch.cuda.set_device(self.args.gpu)
+                    model.cuda(self.args.gpu)
+                    # When using a single GPU per process and per
+                    # DistributedDataParallel, we need to divide the batch size
+                    # ourselves based on the total number of GPUs of the current node.
+                    self.args.batch_size = int(self.args.batch_size / ngpus_per_node)
+                    self.args.workers = int((self.args.workers + ngpus_per_node - 1) / ngpus_per_node)
+                    if self.args.compiled == 1:
+                        model = torch.compile(model)   
+
+                    s = torch.cuda.Stream()
+                    s.wait_stream(torch.cuda.current_stream())
+                    with torch.cuda.stream(s):
+                        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[self.args.gpu])
+                    torch.cuda.current_stream().wait_stream(s)
+                else:
+                    model.cuda()
+
+                    if self.args.compiled == 1:
+                        model = torch.compile(model)   
+
+                    # DistributedDataParallel will divide and allocate batch_size to all
+                    # available GPUs if device_ids are not set
+                    model = torch.nn.parallel.DistributedDataParallel(model)
+        elif self.args.gpu is not None and torch.cuda.is_available():
+            torch.cuda.set_device(self.args.gpu)
+            model = model.cuda(self.args.gpu)
+        elif torch.backends.mps.is_available():
+            device = torch.device("mps")
+            model = model.to(device)
+        else:
+            # DataParallel will divide and allocate batch_size to all available GPUs
+            if self.args.arch.startswith('alexnet') or self.args.arch.startswith('vgg'):
+                model.features = torch.nn.DataParallel(model.features)
+                model.cuda()
+            else:
+                model = torch.nn.DataParallel(model).cuda()
+
+        return model
