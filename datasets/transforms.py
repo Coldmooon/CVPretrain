308730977368264993fe73c228c2353fd53e5850
Coldmooon <howtoliy@gmail.com>
2023-11-20 17:04:36 +0800

    Set mixup alpha = 0.2 according to Resnet-D

--- datasets/transforms.py
+++ datasets/transforms.py
@@ -2,7 +2,7 @@ from torchvision.transforms import v2
 
 def datamix(images, target):
     # cutmix = v2.CutMix(num_classes=1000)
-    mixup = v2.MixUp(num_classes=1000)
+    mixup = v2.MixUp(alpha=0.2, num_classes=1000)
     # cutmix_or_mixup = v2.RandomChoice([cutmix, mixup])
     # converted to one-hot targets
     images, target = mixup(images, target)
