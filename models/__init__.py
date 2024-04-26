154b3ad0f5bb1005ef9e16fd0a39912bbd912ce4
Coldmooon <howtoliy@gmail.com>
2024-04-26 19:11:40 +0800

    model import restricted by __all__

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,2 @@
-from .resnetd import resnet50d
+from .resnetd import * # restricted by __all__ in resnetd.py
 from torchvision.models import resnet18, resnet34, resnet50, resnet101, resnet152, resnext50_32x4d, resnext101_32x8d, resnext101_64x4d , shufflenet_v2_x0_5 , shufflenet_v2_x1_0 , shufflenet_v2_x1_5 , shufflenet_v2_x2_0 , squeezenet1_0 , squeezenet1_1 , swin_b , swin_s , swin_t , swin_v2_b , swin_v2_s , swin_v2_t , vgg11 , vgg11_bn , vgg13 , vgg13_bn , vgg16 , vgg16_bn , vgg19 , vgg19_bn , vit_b_16 , vit_b_32 , vit_h_14 , vit_l_16 , vit_l_32 , wide_resnet101_2 , wide_resnet50_2
